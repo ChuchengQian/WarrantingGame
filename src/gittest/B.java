2cae3a54aa130f84f1ada47fb19e623b00790c51
Bob <u6360655@anu.edu.au>
2018-03-21 17:11:54 +1100

    Experimental change to B

--- src/gittest/B.java
+++ src/gittest/B.java
@@ -1,6 +1,9 @@
 package gittest;
 
 public class B {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
     @Override
     public String toString() {
         return super.toString();
