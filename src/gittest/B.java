744aa71bf767a054f573dbab7ef72e0e9ee5dd8a
Bob <u6360655@anu.edu.au>
2018-03-21 17:12:38 +1100

    Experimental change to B

--- src/gittest/B.java
+++ src/gittest/B.java
@@ -2,7 +2,7 @@ package gittest;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello, I am Bob");
     }
     @Override
     public String toString() {
