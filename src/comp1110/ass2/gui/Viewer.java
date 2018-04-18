fd1fd79d91383de6ffe02453f5f92eeedfb2f35d
songchx <u6302158@anu.edu.au>
2018-04-18 12:44:26 +1000

    T4- replace with lambda

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -72,21 +72,18 @@ public class Viewer extends Application {
         textField = new TextField();
         textField.setPrefWidth(300);
         Button button = new Button("Refresh");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                controls.getChildren().clear();
-                makePlacement(textField.getText());
-                controls.getChildren().add(grid);
-                HBox hb = new HBox();
-                hb.getChildren().addAll(label1, textField, button);
-                hb.setSpacing(10);
-                hb.setLayoutX(130);
-                hb.setLayoutY(VIEWER_HEIGHT - 50);
-                controls.getChildren().add(hb);
-                textField.clear();
+        button.setOnAction(e -> {
+            controls.getChildren().clear();
+            makePlacement(textField.getText());
+            controls.getChildren().add(grid);
+            HBox hb = new HBox();
+            hb.getChildren().addAll(label1, textField, button);
+            hb.setSpacing(10);
+            hb.setLayoutX(130);
+            hb.setLayoutY(VIEWER_HEIGHT - 50);
+            controls.getChildren().add(hb);
+            textField.clear();
 
-            }
         });
         grid.setHgap(6);
         grid.setHgap(6);
